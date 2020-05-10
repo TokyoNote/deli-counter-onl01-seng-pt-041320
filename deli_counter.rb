c82baa5d0130894040a89a562e114c52825c6ada
Camiyah Goins <tokyonote9@gmail.com>
2020-05-10 13:22:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number()
+  if |number| = 0
+    puts "The line is currently empty."
+  elsif |number > 0 
+   puts "The line currently has #{number}"
+  end
+end
